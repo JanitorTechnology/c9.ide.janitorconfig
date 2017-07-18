6e806fff30dcded11626e792b465ed1d00e7d350
Tim Nguyen <ntim.bugs@gmail.com>
2017-07-18 15:48:11 +0000

    Initial commit

---
+++ plugin.js
@@ -0,0 +1,77 @@
+define(function(require, exports, module) {
+    main.consumes = ["settings", "Plugin", "fs"];
+    main.provides = ["janitorconfig"];
+    return main;
+
+    function main(options, imports, register) {
+        var settings = imports.settings;
+        var Plugin = imports.Plugin;
+        var fs = imports.fs;
+
+        /***** Initialization *****/
+        
+        var plugin = new Plugin("janitorconfig", main.consumes);
+        var emit = plugin.getEmitter();
+        
+        function readJanitorManifest() {
+            const filePath = "~/janitor.json";
+            
+            return new Promise((resolve, reject) => {
+                fs.readFile(filePath, function(err, data) {
+                    if (err) reject(err);
+                    resolve(data);
+                });
+            });
+        }
+        
+        function load() {
+            readJanitorManifest().then(manifest => {
+                console.log(JSON.parse(manifest))
+                loadSettings(JSON.parse(manifest).scripts);
+            }).catch(err => console.error("plugin config", err));
+        }
+        function loadSettings(scripts) {
+            let c9runners = {};
+            let i = 0;
+            console.log(scripts)
+            for (let script in scripts) {
+                c9runners[script] = {
+                    "command": scripts[script].cmd,
+                    "cwd": scripts[script].cwd || "/",
+                    "name": script,
+                    "runner": "Shell command",
+                    "toolbar": true,
+                }
+                if (i === 0) {
+                    c9runners[script].default = true;
+                }
+                i++;
+            }
+            console.log(c9runners);
+            settings.setJson("project/run/configs", c9runners);
+        }
+        
+        /***** Methods *****/
+        
+        load();
+        
+        /***** Lifecycle *****/
+        
+        plugin.on("load", function() {
+            load();
+        });
+        plugin.on("unload", function() {
+        
+        });
+        
+        /***** Register and define API *****/
+        
+        plugin.freezePublicAPI({
+            
+        });
+        
+        register(null, {
+            "janitorconfig": plugin
+        });
+    }
+});
