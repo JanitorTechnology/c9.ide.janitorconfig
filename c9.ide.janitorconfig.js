628f520e5cca02f77de286f3e10ae4c74e38a717
Tim Nguyen <ntim.bugs@gmail.com>
2017-07-19 15:34:10 +0000

    Fix #1 and fix #2

--- c9.ide.janitorconfig.js
+++ c9.ide.janitorconfig.js
@@ -14,32 +14,68 @@ define(function(require, exports, module) {
         var emit = plugin.getEmitter();
         
         function readJanitorManifest() {
-            const filePath = "~/janitor.json";
+            const filePaths = [
+                "~/janitor.json",
+                "~/.janitor.json",
+                "/janitor.json",
+                "/.janitor.json"
+            ];
             
-            return new Promise((resolve, reject) => {
-                fs.readFile(filePath, function(err, data) {
-                    if (err) reject(err);
-                    resolve(data);
+            let readFile = function (path) {
+                return new Promise((resolve, reject) => {
+                    fs.readFile(path, function(err, data) {
+                        if (err) {
+                            resolve(undefined);
+                            return;
+                        }
+                        resolve(data);
+                    });
                 });
+            };
+            
+            let promises = Promise.all(filePaths.map(path => readFile(path)));
+            return promises.then(files => {
+                files = files.filter(v => !!v);
+                if (files.length > 1) {
+                    console.log(files);
+                    throw new Error("[c9.ide.janitorconfig] More than one janitor.json file detected.")
+                } else if (files.length === 1) {
+                    return JSON.parse(files[0]);
+                } else {
+                    throw new Error("[c9.ide.janitorconfig] janitor.json file not found")
+                }
             });
         }
         
         function load() {
             readJanitorManifest().then(manifest => {
-                loadSettings(JSON.parse(manifest).scripts);
-            }).catch(err => console.error("plugin config", err));
+                loadSettings(manifest.scripts);
+            }).catch(err => {
+                console.error("[c9.ide.janitorconfig] Failed to parse or load janitor.json", err)
+            });
         }
         function loadSettings(scripts) {
             let c9runners = {};
             let i = 0;
-            console.log(scripts)
             for (let script in scripts) {
-                c9runners[script] = {
-                    "command": scripts[script].cmd,
-                    "cwd": scripts[script].cwd || "/",
-                    "name": script,
-                    "runner": "Shell command",
-                    "toolbar": true,
+                if (scripts[script].cmd) {
+                    c9runners[script] = {
+                        "command": scripts[script].cmd,
+                        "cwd": scripts[script].cwd || "/",
+                        "name": script,
+                        "runner": "Shell command",
+                        "toolbar": true,
+                    };
+                } else if (typeof scripts[script] === "string") {
+                    c9runners[script] = {
+                        "command": scripts[script],
+                        "cwd": "/",
+                        "name": script,
+                        "runner": "Shell command",
+                        "toolbar": true,
+                    };
+                } else {
+                    console.error("[c9.ide.janitorconfig]", "Failed to parse script", script);
                 }
                 if (i === 0) {
                     c9runners[script].default = true;
@@ -47,8 +83,9 @@ define(function(require, exports, module) {
                 i++;
             }
             settings.setJson("project/run/configs", c9runners);
+            console.info("[c9.ide.janitorconfig] Finished loading janitor.json scripts.");
         }
-        
+
         /***** Lifecycle *****/
         
         plugin.on("load", function() {
@@ -57,7 +94,7 @@ define(function(require, exports, module) {
         plugin.on("unload", function() {
         
         });
-        
+
         /***** Register and define API *****/
         
         plugin.freezePublicAPI({
